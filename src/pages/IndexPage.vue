601ebfacb569662050277bfc1a5288b22c16e9f9
hatBox <allangonzalez200@gmail.com>
2022-06-14 16:36:53 -0600

    Test app

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-center">
+
+        <q-btn color="white" text-color="black" @click="scanDocument" label="Scanear documento"></q-btn>
+  
+
+
+  </q-page>
+</template>
+
+<script>
+
+export default{
+  methods:{
+
+  },
+  mounted(){
+    console.log(scan)
+  }
+  
+}
+</script>
